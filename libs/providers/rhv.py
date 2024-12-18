cc791047b93548a689c3d0a92e254060cf03d462
Meni Yakove <myakove@gmail.com>
2024-12-18 09:44:44 +0200

    add from __future__ import annotations

--- libs/providers/rhv.py
+++ libs/providers/rhv.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import Any
 import ovirtsdk4
 import copy
--- utilities/mtv_migration.py
+++ utilities/mtv_migration.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import Any, Generator
 import uuid
 from contextlib import contextmanager
