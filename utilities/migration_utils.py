2ceaefdffa32142622e5fd2d90da315d516deb73
Meni Yakove <441263+myakove@users.noreply.github.com>
2025-02-10 14:04:31 +0200

    wait for all resources to be deleted after cancel migration and archived plans. (#37)
    * Make sure namespaces are deleted last
    
    * Make sure namespaces are deleted last
    
    * Make sure dv,pvc and pods are deleted when migration is canceled
    
    * Make sure dv,pvc and pods are deleted when migration is canceled
    
    * Log error if pv is not cleaned up as it should after migration is canceled
    
    * Fix cancle wait for condition
    
    * Use migration client in migration for loop
    
    * make sure PersistentVolumeClaim is deleted after cancel migration
    
    * make sure PersistentVolumeClaim is deleted after cancel migration
    
    * Use multiprocessing to ru function on background
    
    * Use multiprocessing to ru function on background
    
    * Log when resources delete failed
    
    * Code fixes
    
    * Cancel migration only if it's in state executing
    Check all needed resources gone after cancel/archive migratopn/plan
    
    * save leftovers when call it
    
    * Fix typing
    
    * Call cancel migration and archive plan when it fail, not at the end
    
    * Do not call teardown if passing --collectony or --setup-plan to pytest
    
    * Remove useless constant _kind, use it directlly

---
+++ utilities/migration_utils.py
@@ -0,0 +1,121 @@
+import contextlib
+
+from kubernetes.dynamic import DynamicClient
+from kubernetes.dynamic.exceptions import NotFoundError
+from ocp_resources.datavolume import DataVolume
+from ocp_resources.migration import Migration
+from ocp_resources.persistent_volume import PersistentVolume
+from ocp_resources.persistent_volume_claim import PersistentVolumeClaim
+from ocp_resources.plan import Plan
+from ocp_resources.pod import Pod
+from ocp_resources.resource import NamespacedResource, Resource, ResourceEditor
+from simple_logger.logger import get_logger
+from timeout_sampler import TimeoutExpiredError
+
+LOGGER = get_logger(__name__)
+
+
+def cancel_migration(migration: Migration) -> None:
+    for condition in migration.instance.status.conditions:
+        # Only cancel migrations that are in "Executing" state
+        if condition.type == migration.Condition.Type.RUNNING and condition.status == migration.Condition.Status.TRUE:
+            LOGGER.info(f"Canceling migration {migration.name}")
+
+            migration_spec = migration.instance.spec
+            plan = Plan(client=migration.client, name=migration_spec.plan.name, namespace=migration_spec.plan.namespace)
+            plan_instance = plan.instance
+
+            ResourceEditor(
+                patches={
+                    migration: {
+                        "spec": {
+                            "cancel": plan_instance.spec.vms,
+                        }
+                    }
+                }
+            ).update()
+
+            try:
+                migration.wait_for_condition(
+                    condition=migration.Condition.CANCELED, status=migration.Condition.Status.TRUE
+                )
+                check_dv_pvc_pv_deleted(
+                    ocp_client=migration.client,
+                    target_namespace=plan.instance.spec.targetNamespace,
+                    partial_name=migration.name,
+                )
+            except TimeoutExpiredError:
+                LOGGER.error(f"Failed to cancel migration {migration.name}")
+                return
+
+
+def archive_plan(plan: Plan) -> None:
+    LOGGER.info(f"Archiving plan {plan.name}")
+
+    ResourceEditor(
+        patches={
+            plan: {
+                "spec": {
+                    "archived": True,
+                }
+            }
+        }
+    ).update()
+
+    try:
+        plan.wait_for_condition(condition=plan.Condition.ARCHIVED, status=plan.Condition.Status.TRUE)
+        for _pod in Pod.get(dyn_client=plan.client, namespace=plan.instance.spec.targetNamespace):
+            if plan.name in _pod.name:
+                if not _pod.wait_deleted():
+                    LOGGER.error(f"Pod was not deleted after plan {plan.name} was archived")
+
+    except TimeoutExpiredError:
+        LOGGER.error(f"Failed to archive plan {plan.name}")
+        return
+
+
+def check_dv_pvc_pv_deleted(
+    ocp_client: DynamicClient,
+    target_namespace: str,
+    partial_name: str,
+    leftovers: dict[str, list[dict[str, str]]] | None = None,
+) -> dict[str, list[dict[str, str]]]:
+    # When migration in not canceled (succeeded) DVs,PVCs are deleted only when the target_namespace is deleted
+    # All calls wrap with `with contextlib.suppress(NotFoundError)` since the resources can be gone even after we get it.
+    for _dv in DataVolume.get(dyn_client=ocp_client, namespace=target_namespace):
+        with contextlib.suppress(NotFoundError):
+            if partial_name in _dv.name:
+                if not _dv.wait_deleted():
+                    if leftovers:
+                        leftovers = append_leftovers(leftovers=leftovers, resource=_dv)
+
+    for _pvc in PersistentVolumeClaim.get(dyn_client=ocp_client, namespace=target_namespace):
+        with contextlib.suppress(NotFoundError):
+            if partial_name in _pvc.name:
+                if not _pvc.wait_deleted():
+                    if leftovers:
+                        leftovers = append_leftovers(leftovers=leftovers, resource=_pvc)
+
+    for _pv in PersistentVolume.get(dyn_client=ocp_client):
+        with contextlib.suppress(NotFoundError):
+            _pv_spec = _pv.instance.spec.to_dict()
+            if partial_name in _pv_spec.get("claimRef", {}).get("name", ""):
+                if not _pv.wait_deleted():
+                    if leftovers:
+                        leftovers = append_leftovers(leftovers=leftovers, resource=_pv)
+
+    return leftovers or {}
+
+
+def append_leftovers(
+    leftovers: dict[str, list[dict[str, str]]], resource: Resource | NamespacedResource
+) -> dict[str, list[dict[str, str]]]:
+    _name = resource.name
+    _namespace = resource.namespace
+
+    leftovers.setdefault(resource.kind, []).append({
+        "name": _name,
+        "namespace": _namespace,
+    })
+
+    return leftovers
--- utilities/pytest_utils.py
+++ utilities/pytest_utils.py
@@ -2,107 +2,173 @@ import json
 from pathlib import Path
 from typing import Any
 
+from kubernetes.dynamic import DynamicClient
+from ocp_resources.host import Host
 from ocp_resources.migration import Migration
+from ocp_resources.namespace import Namespace
+from ocp_resources.network_attachment_definition import NetworkAttachmentDefinition
+from ocp_resources.network_map import NetworkMap
 from ocp_resources.plan import Plan
-from ocp_resources.resource import ResourceEditor
+from ocp_resources.pod import Pod
+from ocp_resources.provider import Provider
+from ocp_resources.resource import get_client
+from ocp_resources.secret import Secret
+from ocp_resources.storage_map import StorageMap
+from ocp_resources.virtual_machine import VirtualMachine
 from simple_logger.logger import get_logger
-from timeout_sampler import TimeoutExpiredError
+
+from utilities.migration_utils import append_leftovers, archive_plan, cancel_migration, check_dv_pvc_pv_deleted
 
 LOGGER = get_logger(__name__)
 
 
-def session_teardown(session_store: dict[str, Any]) -> None:
-    LOGGER.info("Running teardown to delete all created resources")
-    session_teardown_resources = session_store["teardown"]
-
-    try:
-        cancel_migrations(migrations=session_teardown_resources.get("Migration", []))
-        archive_plans(plans=session_teardown_resources.get("Plan", []))
-
-    finally:
-        for _resource_list in session_teardown_resources.values():
-            for _resource in _resource_list:
-                try:
-                    _resource.clean_up(wait=True)
-                except Exception as ex:
-                    LOGGER.error(f"Failed to clean up {_resource.name} due to: {ex}")
-
-
 def collect_created_resources(session_store: dict[str, Any], data_collector_path: Path) -> None:
-    _created_reousrces: dict[str, list[dict[str, str]]] = {}
+    """
+    collect created resources and store them in resource.json file under data collector path
+    """
+    resources = session_store["teardown"]
 
-    for _resource_kind, _resource_list in session_store["teardown"].items():
-        _created_reousrces.setdefault(_resource_kind, [])
-        for _resource in _resource_list:
-            LOGGER.info(f"Collecting data for resource {_resource.name}")
-            try:
-                _created_reousrces[_resource_kind].append({
-                    "module": _resource.__module__,
-                    "name": _resource.name,
-                    "namespace": _resource.namespace,
-                })
-
-            except Exception as ex:
-                LOGGER.error(f"Failed to collect data for resource {_resource.name} data due to: {ex}")
-
-    if _created_reousrces:
+    if resources:
         try:
             LOGGER.info(f"Write created resources data to {data_collector_path}/resources.json")
             with open(data_collector_path / "resources.json", "w") as fd:
-                json.dump(_created_reousrces, fd)
+                json.dump(session_store["teardown"], fd)
 
         except Exception as ex:
             LOGGER.error(f"Failed to store resources.json due to: {ex}")
 
 
-def cancel_migrations(migrations: list[Migration]) -> None:
-    migrations_to_cancel: list[Migration] = migrations
+def session_teardown(session_store: dict[str, Any]) -> None:
+    LOGGER.info("Running teardown to delete all created resources")
 
+    ocp_client = get_client()
+    session_teardown_resources = session_store["teardown"]
+    target_namespace = session_store["target_namespace"]
+    session_uuid = session_store["session_uuid"]
+    leftovers: dict[str, list[dict[str, str]]] = {}
+
+    for migration_name in session_teardown_resources.get(Migration.kind, []):
+        migration = Migration(name=migration_name["name"], namespace=migration_name["namespace"], client=ocp_client)
+        cancel_migration(migration=migration)
+
+    for plan_name in session_teardown_resources.get(Plan.kind, []):
+        plan = Plan(name=plan_name["name"], namespace=plan_name["namespace"], client=ocp_client)
+        archive_plan(plan=plan)
+
+    teardown_resources(
+        session_teardown_resources=session_teardown_resources,
+        ocp_client=ocp_client,
+        target_namespace=target_namespace,
+        session_uuid=session_uuid,
+        leftovers=leftovers,
+    )
+
+
+def teardown_resources(
+    session_teardown_resources: dict[str, list[dict[str, str]]],
+    ocp_client: DynamicClient,
+    session_uuid: str,
+    target_namespace: str,
+    leftovers: dict[str, list[dict[str, str]]],
+) -> bool:
+    """
+    Delete all the resources that was created by the tests.
+    Check that resources that was created by the migration is deleted
+    Report if we have any leftovers in the cluster and return False if any, else return True
+    """
+    # Resources that was created by the tests
+    migrations = session_teardown_resources.get(Migration.kind, [])
+    plans = session_teardown_resources.get(Plan.kind, [])
+    providers = session_teardown_resources.get(Provider.kind, [])
+    hosts = session_teardown_resources.get(Host.kind, [])
+    secrets = session_teardown_resources.get(Secret.kind, [])
+    network_attachment_definitions = session_teardown_resources.get(NetworkAttachmentDefinition.kind, [])
+    networkmaps = session_teardown_resources.get(NetworkMap.kind, [])
+    namespaces = session_teardown_resources.get(Namespace.kind, [])
+    storagemaps = session_teardown_resources.get(StorageMap.kind, [])
+
+    # Resources that was created by running migration
+    pods = session_teardown_resources.get(Pod.kind, [])
+    virtual_machines = session_teardown_resources.get(VirtualMachine.kind, [])
+
+    # Clean all resources that was created by the tests
     for migration in migrations:
-        for condition in migration.instance.status.conditions:
-            # No need to cancel migration if it's already succeeded
-            if (
-                condition.type == migration.Condition.Type.SUCCEEDED
-                and condition.status == migration.Condition.Status.TRUE
-            ):
-                migrations_to_cancel.remove(migration)
-                break
+        migration_obj = Migration(name=migration["name"], namespace=migration["namespace"], client=ocp_client)
+        if not migration_obj.clean_up(wait=True):
+            leftovers = append_leftovers(leftovers=leftovers, resource=migration_obj)
 
-    for migration in migrations_to_cancel:
-        LOGGER.info(f"Canceling migration {migration.name}")
-        migration_spec = migration.instance.spec
-        plan = Plan(client=migration.client, name=migration_spec.plan.name, namespace=migration_spec.plan.namespace)
-        ResourceEditor(
-            patches={
-                migration: {
-                    "spec": {
-                        "cancel": plan.instance.spec.vms,
-                    }
-                }
-            }
-        ).update()
-
-        try:
-            plan.wait_for_condition(condition="Canceled", status=plan.Condition.Status.TRUE)
-        except Exception:
-            LOGGER.error(f"Failed to cancel migration {migration.name}")
-
-
-def archive_plans(plans: list[Plan]) -> None:
     for plan in plans:
-        LOGGER.info(f"Archiving plan {plan.name}")
+        plan_obj = Plan(name=plan["name"], namespace=plan["namespace"], client=ocp_client)
+        if not plan_obj.clean_up(wait=True):
+            leftovers = append_leftovers(leftovers=leftovers, resource=plan_obj)
 
-        ResourceEditor(
-            patches={
-                plan: {
-                    "spec": {
-                        "archived": True,
-                    }
-                }
-            }
-        ).update()
+    for provider in providers:
+        provider_obj = Provider(name=provider["name"], namespace=provider["namespace"], client=ocp_client)
+        if not provider_obj.clean_up(wait=True):
+            leftovers = append_leftovers(leftovers=leftovers, resource=provider_obj)
 
-        try:
-            plan.wait_for_condition(condition="Archived", status=plan.Condition.Status.TRUE)
-        except TimeoutExpiredError:
-            LOGGER.error(f"Failed to archive plan {plan.name}")
+    for host in hosts:
+        host_obj = Host(name=host["name"], namespace=host["namespace"], client=ocp_client)
+        if not host_obj.clean_up(wait=True):
+            leftovers = append_leftovers(leftovers=leftovers, resource=host_obj)
+
+    for secret in secrets:
+        secret_obj = Secret(name=secret["name"], namespace=secret["namespace"], client=ocp_client)
+        if not secret_obj.clean_up(wait=True):
+            leftovers = append_leftovers(leftovers=leftovers, resource=secret_obj)
+
+    for network_attachment_definition in network_attachment_definitions:
+        network_attachment_definition_obj = NetworkAttachmentDefinition(
+            name=network_attachment_definition["name"],
+            namespace=network_attachment_definition["namespace"],
+            client=ocp_client,
+        )
+        if not network_attachment_definition_obj.clean_up(wait=True):
+            leftovers = append_leftovers(leftovers=leftovers, resource=network_attachment_definition_obj)
+
+    for storagemap in storagemaps:
+        storagemap_obj = StorageMap(name=storagemap["name"], namespace=storagemap["namespace"], client=ocp_client)
+        if not storagemap_obj.clean_up(wait=True):
+            leftovers = append_leftovers(leftovers=leftovers, resource=storagemap_obj)
+
+    for networkmap in networkmaps:
+        networkmap_obj = NetworkMap(name=networkmap["name"], namespace=networkmap["namespace"], client=ocp_client)
+        if not networkmap_obj.clean_up(wait=True):
+            leftovers = append_leftovers(leftovers=leftovers, resource=networkmap_obj)
+
+    for namespace in namespaces:
+        namespace_obj = Namespace(name=namespace["name"], client=ocp_client)
+        if not namespace_obj.clean_up(wait=True):
+            leftovers = append_leftovers(leftovers=leftovers, resource=namespace_obj)
+
+    # Check that resources that was created by running migration are deleted
+    for virtual_machine in virtual_machines:
+        virtual_machine_obj = VirtualMachine(
+            name=virtual_machine["name"], namespace=virtual_machine["namespace"], client=ocp_client
+        )
+        if virtual_machine_obj.exists:
+            if not virtual_machine_obj.clean_up(wait=True):
+                leftovers = append_leftovers(leftovers=leftovers, resource=virtual_machine_obj)
+
+    for pod in pods:
+        pod_obj = Pod(name=pod["name"], namespace=pod["namespace"], client=ocp_client)
+        if pod_obj.exists:
+            if not pod_obj.clean_up(wait=True):
+                leftovers = append_leftovers(leftovers=leftovers, resource=pod_obj)
+
+    # Make sure all pods related to the test session are deleted
+    for _pod in Pod.get(dyn_client=ocp_client, namespace=target_namespace):
+        if session_uuid in _pod.name:
+            if not _pod.wait_deleted():
+                leftovers = append_leftovers(leftovers=leftovers, resource=_pod)
+
+    leftovers = check_dv_pvc_pv_deleted(
+        leftovers=leftovers, ocp_client=ocp_client, target_namespace=target_namespace, partial_name=session_uuid
+    )
+
+    # Report if we have any leftovers
+    if leftovers:
+        LOGGER.error(f"Failed to clean up the following resources: {leftovers}")
+        return False
+
+    return True
