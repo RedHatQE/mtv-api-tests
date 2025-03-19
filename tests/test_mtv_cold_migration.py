7cf650bf47495259e6abdb57bc6648ad70e4a3d7
Meni Yakove <myakove@gmail.com>
2025-03-19 11:02:25 +0200

    wip: run remote test as local

--- tests/test_mtv_cold_migration.py
+++ tests/test_mtv_cold_migration.py
@@ -1,7 +1,6 @@
 import pytest as pytest
 
 from utilities.mtv_migration import get_vm_suffix, migrate_vms
-from utilities.utils import get_value_from_py_config
 
 VM_SUFFIX = get_vm_suffix()
 
@@ -50,6 +49,56 @@ def test_sanity_cold_mtv_migration(
     )
 
 
+# @pytest.mark.remote
+# @pytest.mark.parametrize(
+#     "plans",
+#     [
+#         # MTV-79
+#         pytest.param(
+#             [
+#                 {
+#                     "virtual_machines": [
+#                         {"name": f"mtv-rhel8-79{VM_SUFFIX}"},
+#                         {
+#                             "name": f"mtv-win2019-79{VM_SUFFIX}",
+#                         },
+#                     ],
+#                     "warm_migration": False,
+#                 }
+#             ],
+#             # TODO fix Polarion ID
+#         )
+#     ],
+#     indirect=True,
+#     ids=["MTV-79"],
+# )
+# @pytest.mark.skipif(not get_value_from_py_config("remote_ocp_cluster"), reason="No remote OCP cluster provided")
+# def test_cold_remote_ocp(
+#     request,
+#     fixture_store,
+#     session_uuid,
+#     target_namespace,
+#     plans,
+#     source_provider,
+#     source_provider_data,
+#     destination_provider,
+#     network_migration_map,
+#     storage_migration_map,
+# ):
+#     migrate_vms(
+#         fixture_store=fixture_store,
+#         test_name=request._pyfuncitem.name,
+#         session_uuid=session_uuid,
+#         source_provider=source_provider,
+#         destination_provider=destination_provider,
+#         plans=plans,
+#         network_migration_map=network_migration_map,
+#         storage_migration_map=storage_migration_map,
+#         source_provider_data=source_provider_data,
+#         target_namespace=target_namespace,
+#     )
+
+
 @pytest.mark.remote
 @pytest.mark.parametrize(
     "plans",
@@ -67,13 +116,11 @@ def test_sanity_cold_mtv_migration(
                     "warm_migration": False,
                 }
             ],
-            # TODO fix Polarion ID
         )
     ],
     indirect=True,
     ids=["MTV-79"],
 )
-@pytest.mark.skipif(not get_value_from_py_config("remote_ocp_cluster"), reason="No remote OCP cluster provided")
 def test_cold_remote_ocp(
     request,
     fixture_store,
@@ -82,19 +129,19 @@ def test_cold_remote_ocp(
     plans,
     source_provider,
     source_provider_data,
-    destination_ocp_provider,
-    remote_network_migration_map,
-    remote_storage_migration_map,
+    destination_provider,
+    network_migration_map,
+    storage_migration_map,
 ):
     migrate_vms(
         fixture_store=fixture_store,
         test_name=request._pyfuncitem.name,
         session_uuid=session_uuid,
         source_provider=source_provider,
-        destination_provider=destination_ocp_provider,
+        destination_provider=destination_provider,
         plans=plans,
-        network_migration_map=remote_network_migration_map,
-        storage_migration_map=remote_storage_migration_map,
+        network_migration_map=network_migration_map,
+        storage_migration_map=storage_migration_map,
         source_provider_data=source_provider_data,
         target_namespace=target_namespace,
     )
--- tools/dev/build_run_tests_command.py
+++ tools/dev/build_run_tests_command.py
@@ -57,7 +57,7 @@ def main() -> str:
     base_cmd = (
         f"uv run pytest -s --tc=matrix_test:true --tc=target_ocp_version:{cluster_version}"
         f" --tc=insecure_verify_skip:true --tc=mount_root:{os.environ['MOUNT_PATH']}"
-        " --skip-data-collector"
+        # " --skip-data-collector"
     )
 
     if len(sys.argv) < 2:
