7dc416107a6ee55796844663772d8fe32ec4add3
Meni Yakove <myakove@gmail.com>
2025-03-24 10:55:56 +0200

    Move to new vCenter 8

--- tests/tests_config/config.py
+++ tests/tests_config/config.py
@@ -63,8 +63,8 @@ source_providers_list = [
     {
         "type": "vsphere",
         "version": "8.0.1",
-        "fqdn": "10.6.46.249",
-        "api_url": "https://10.6.46.249/sdk",
+        "fqdn": "10.6.46.250",
+        "api_url": "https://10.6.46.250/sdk",
         "username": "administrator@vsphere.local",
         "password": "VCENTER@redhat2023",
         "admin_username": "administrator@vsphere.local",
