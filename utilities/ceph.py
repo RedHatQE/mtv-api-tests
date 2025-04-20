1effe430667f0bcc25521110f1e30de8bce3767e
Meni Yakove <myakove@gmail.com>
2025-04-20 11:19:44 +0300

    Do not fail if Prometheus failed to init

--- utilities/ceph.py
+++ utilities/ceph.py
@@ -2,7 +2,6 @@ import shlex
 import time
 
 from kubernetes.dynamic import DynamicClient
-from ocp_resources.pod import Pod
 from ocp_utilities.monitoring import Prometheus
 from pyhelper_utils.shell import run_command
 from simple_logger.logger import get_logger
@@ -10,10 +9,15 @@ from simple_logger.logger import get_logger
 LOGGER = get_logger(__name__)
 
 
-def ceph_monitor_deamon(ocp_admin_client: DynamicClient, ceph_tools_pod: Pod) -> None:
+def ceph_monitor_deamon(ocp_admin_client: DynamicClient) -> None:
     token_command = "oc create token prometheus-k8s -n openshift-monitoring --duration=999999s"
     _, token, _ = run_command(command=shlex.split(token_command), verify_stderr=False)
-    prometheus = Prometheus(client=ocp_admin_client, verify_ssl=False, bearer_token=token)
+    try:
+        prometheus = Prometheus(client=ocp_admin_client, verify_ssl=False, bearer_token=token)
+    except Exception as exp:
+        LOGGER.warning(f"Failed to get Prometheus client. {exp}")
+        return
+
     while True:
         alerts = prometheus.get_firing_alerts(alert_name="CephOSDCriticallyFull")
         if alerts:
