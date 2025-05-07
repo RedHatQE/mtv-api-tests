a3734296c3b614902ac3a91db6de663fe3455484
Meni Yakove <myakove@gmail.com>
2025-05-07 15:39:19 +0300

    Run Jenkins jobs from cli

--- tools/jenkins-runner.py
+++ tools/jenkins-runner.py
@@ -18,11 +18,7 @@ def main(
 ) -> None:
     os.environ["PYTHONHTTPSVERIFY"] = "0"
     url = "https://jenkins-csb-mtv-qe-main.dno.corp.redhat.com"
-    api = jenkins_api.Jenkins(
-        url=url,
-        username=os.environ["JENKINS_USERNAME"],
-        password=os.environ["JENKINS_PASSWORD"],
-    )
+    api = jenkins_api.Jenkins(url=url)
 
     supported_jobs: list[str] = [
         "mtv-2.8-ocp-4.18-test-release-gate",
@@ -60,11 +56,11 @@ def main(
         "DEPLOY_NFS_CSI": deploy_ocp,
     }
     if iib:
-        iib_dict = get_mtv_latest_iib(version=mtv_version)[ocp_version]
+        iib_dict = get_mtv_latest_iib(version=mtv_version)[f"v{ocp_version}"]
 
         params["DEPLOY_MTV"] = True
         params["IIB_NO"] = iib_dict["IIB"]
-        params["MTV_VERSION"] = mtv_version
+        params["MTV_VERSION"] = iib_dict["MTV"].split("-")[0]
 
     api.build_job(name=job_name, parameters=params)
     print(f"{url}/{job_name}")
@@ -78,7 +74,7 @@ if __name__ == "__main__":
     parser.add_argument("-j", "--job-name", help="Jenkins job name", required=True)
     parser.add_argument("-c", "--cluster", help="Cluster name", required=True)
     parser.add_argument("--iib", help="install MTV using IIB", action="store_true")
-    parser.add_argument("--deploy-ocp", help="Deploy the OCP cluster", action="store_true")
+    parser.add_argument("--deploy", help="Deploy the OCP cluster", action="store_true")
     parser.add_argument("--branch", help="Git branch", default="main")
     parser.add_argument("--ocp-wrapper-branch", help="OpenShift Python wrapper git branch")
     args = parser.parse_args()
@@ -87,6 +83,7 @@ if __name__ == "__main__":
         job_name=args.job_name,
         cluster=args.cluster,
         iib=args.iib,
+        deploy_ocp=args.deploy,
         git_branch=args.branch,
         openshift_python_wrapper_git_branch=args.ocp_wrapper_branch,
     )
